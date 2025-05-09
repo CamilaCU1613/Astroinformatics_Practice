9140664e261535bc2149ea1513c805b0064027fa
CamilaCU1613 <116864772+CamilaCU1613@users.noreply.github.com>
2025-05-08 21:19:16 -0400

    Add files via upload

---
+++ list_csv.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# This script saves the names of all .csv files to a text file
+
+ls *.csv > csv_file_list.txt
+echo "Created file csv_file_list.txt with the names of the CSV files"
+
+
